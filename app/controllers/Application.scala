fd2251e00e2165f7e6f8768101c9cc8a9c7cd0a3
Naaman Newbold <naaman@heroku.com>
2012-11-26 16:22:52 -0800

    Edit

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,17 +1,72 @@
 package controllers
 
 import play.api.mvc._
-import services.{LibratoUser, RequestFailedException, PlayAppLibratoService}
-import play.api.libs.concurrent.Promise
+import services._
 import play.api.data._
 import play.api.data.Forms._
 import java.util.concurrent.TimeUnit
+import services.LibratoUser
+import services.MetricAttributes
+import play.api.libs.concurrent.Promise
+import play.api.Logger
 
 object Application extends Controller {
 
   def index = LibratoAuthenticated { librato => implicit lu => request =>
-    Promise.pure(Ok(views.html.index("Your new application is ready.")))
+    librato.getAllMetrics.map(metrics => Ok(views.html.index(metrics)))
   }
+
+  def editMetricsAttributes = LibratoAuthenticated(parse.urlFormEncoded) {
+    librato => implicit lu => request =>
+
+    librato.getMetrics(request.body("metric")).map { metrics =>
+      def mergeAttr(left: Option[String], right: Option[String]) =
+        for { l <- left; r <- right if l == r } yield (r)
+
+      def mergeAttributes(left: MetricAttributes, right: MetricAttributes) =
+        MetricAttributes(
+          mergeAttr(left.color, right.color),
+          mergeAttr(left.displayMax, right.displayMax),
+          mergeAttr(left.displayMin, right.displayMin),
+          mergeAttr(left.displayUnitsLong, right.displayUnitsLong),
+          mergeAttr(left.displayUnitsShort, right.displayUnitsShort),
+          mergeAttr(left.displayStacked, right.displayStacked),
+          mergeAttr(left.displayTransform, right.displayTransform)
+        )
+
+      val mergedAttributes = metrics.map(_.attributes).reduceLeft(mergeAttributes(_, _))
+
+      Ok(views.html.editmetric(metrics, attributesForm.fill(mergedAttributes)))
+    }
+  }
+
+  def saveMetricsAttributes = LibratoAuthenticated(parse.urlFormEncoded) {
+    librato => implicit lu => implicit request =>
+
+    val newAttrs = attributesForm.bindFromRequest()
+    Logger.debug("newAttrs: " + newAttrs)
+    librato.putMetricAttributes(request.body("metric"), newAttrs.get).map { m =>
+      Redirect(routes.Application.index)
+    }.recover {
+      case e: Exception =>
+        Logger.error("Unable to save metrics.", e)
+        BadRequest
+    }
+  }
+
+  val attributesForm = Form(
+    mapping(
+      "color" -> optional(text),
+      "displayMax" -> optional(text),
+      "displayMin" -> optional(text),
+      "displayUnitsLong" -> optional(text),
+      "displayUnitsShort" -> optional(text),
+      "displayStacked" -> optional(text),
+      "displayTransform" -> optional(text)
+    )
+    (MetricAttributes.apply)
+    (MetricAttributes.unapply)
+  )
 
 }
 
--- app/services/LibratoService.scala
+++ app/services/LibratoService.scala
@@ -4,9 +4,10 @@ import play.api.libs.ws.WS
 import org.jboss.netty.handler.codec.http.HttpMethod
 import com.ning.http.client.Realm
 import play.api.libs.ws.WS.WSRequestHolder
-import com.codahale.jerkson.Json
-import play.api.http.Status
+import com.codahale.jerkson.{JsonSnakeCase, Json}
+import play.api.http.{Status, HeaderNames}
 import play.api.libs.concurrent.Promise
+import play.api.Logger
 
 /**
  * @author Naaman Newbold
@@ -45,18 +46,25 @@ trait LibratoService {
       else batches :+ ((total / batch) * batch -> total % batch)
     }
 
-    def execBatches = (batches: Seq[(Int, Int)]) => batches.map {
-      case (offset, total) => client.exec(new MetricList(total, offset))
-    }
-
-    def sequenceBatches(batches: Seq[(Int, Int)]) = Promise.sequence(execBatches(batches))
-
     client.exec(new MetricList(1))
       .map(_.query("total"))
       .map(metricSlices(_))
-      .flatMap(sequenceBatches(_))
+      .flatMap(client.collectBatch(_) {
+        case (offset, total) => new MetricList(total, offset)
+      })
       .map(_.map(_.metrics).flatten)
   }
+
+  def getMetrics(metrics: Seq[String]) = {
+    client.collectBatch(metrics)(new MetricInfo(_))
+  }
+
+  def putMetricAttributes(metrics: Seq[String], attrs: MetricAttributes) = {
+    client.collectBatch(metrics) { metricName =>
+      new AttributesUpdate(metricName, attrs)
+    }
+  }
+
 }
 
 trait LibratoRequest[T] {
@@ -76,16 +84,26 @@ trait LibratoConfig {
 trait LibratoClient {
   def config: LibratoConfig
 
+  def collectBatch[T,R](req: Seq[T])(ƒ: T => LibratoRequest[R]) =
+    Promise.sequence(execBatch(req)(ƒ))
+
+  def execBatch[T,R](req: Seq[T])(ƒ: T => LibratoRequest[R]) =
+    req.map(t => exec(ƒ(t)))
+
   def exec[T](implicit req: LibratoRequest[T]): Promise[T] =
     createWS(req).map(r => req.respond(r.status, r.body.getBytes))
 
-  protected def createWS[T] = callout[T] _ compose auth[T] compose url[T]
+  protected def createWS[T] = callout[T] _ compose auth[T] compose headers[T] compose url[T]
+
+  protected def url[T](req: LibratoRequest[T]): (WSRequestHolder, LibratoRequest[T]) =
+    (WS.url(config.protocol + "://" + config.host + "/v1" + req.path), req)
 
   protected def auth[T](r: (WSRequestHolder, LibratoRequest[T])) =
     (r._1.withAuth(config.username, config.password, Realm.AuthScheme.BASIC), r._2)
 
-  protected def url[T](req: LibratoRequest[T]): (WSRequestHolder, LibratoRequest[T]) =
-    (WS.url(config.protocol + "://" + config.host + "/v1" + req.path), req)
+  protected val stdHeaders = Seq(HeaderNames.CONTENT_TYPE -> "application/json")
+  protected def headers[T](r: (WSRequestHolder, LibratoRequest[T])) =
+    (r._1.withHeaders(stdHeaders:_*), r._2)
 
   protected def callout[T](reqs: (WSRequestHolder, LibratoRequest[T])) = {
     val (wsReq, req) = reqs
@@ -98,7 +116,7 @@ trait LibratoClient {
   }
 }
 
-case class MetricQuery(query: Map[String, Int], metrics: Seq[Map[String, Any]])
+case class MetricQuery(query: Map[String, Int], metrics: Seq[LibratoMetric])
 
 class MetricList(length: Int, offset: Int = 0) extends LibratoRequest[MetricQuery] {
   def method = HttpMethod.GET
@@ -110,9 +128,52 @@ class MetricList(length: Int, offset: Int = 0) extends LibratoRequest[MetricQuer
   }
 }
 
+@JsonSnakeCase
+case class LibratoMetric(
+  name: String,
+  displayName: String,
+  `type`: String,
+  description: Option[String],
+  period: Option[String],
+  attributes: MetricAttributes,
+  measurements: Option[Map[String, Any]]
+)
+
+@JsonSnakeCase
+case class MetricAttributes(
+  color: Option[String],
+  displayMax: Option[String],
+  displayMin: Option[String],
+  displayUnitsLong: Option[String],
+  displayUnitsShort: Option[String],
+  displayStacked: Option[String],
+  displayTransform: Option[String]
+)
+
+class MetricInfo(name: String) extends LibratoRequest[LibratoMetric] {
+  def method = HttpMethod.GET
+  def path = "/metrics/" + name
+  def body = None
+  def respond(status: Int, data: Array[Byte]) = {
+    if (status == Status.OK) Json.parse[LibratoMetric](data)
+    else throw new RequestFailedException(status, data)
+  }
+}
+
+class AttributesUpdate(metricName: String, attrs: MetricAttributes)
+  extends LibratoRequest[Unit] {
+  def method = HttpMethod.PUT
+  def path = "/metrics/" + metricName
+  def body = Some(Json.generate(Map("attributes" -> attrs)))
+  def respond(status: Int, data: Array[Byte]) = {
+    if (status == Status.NO_CONTENT) Unit
+    else throw new RequestFailedException(status, data)
+  }
+}
+
 class RequestFailedException(status: Int, body: Array[Byte])
   extends RuntimeException(
-    "Request failed: Status=%d, Body=%s".format(status, String.valueOf(body))
+    "Request failed: Status=%d, Body=%s".format(status, new String(body))
   ) {
   val getStatus = status
   val getBody = body
